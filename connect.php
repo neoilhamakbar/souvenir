14e4e883ec0b838b1f2f27675b6e6888e578b3c2
neoilhamakbar <50211523+neoilhamakbar@users.noreply.github.com>
2019-11-10 21:35:27 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,6 +2,6 @@
 	$host = "localhost";
 	$user = "root";
 	$pass = "1qaz2wsx3edc@";
-	$dbname = "bkt_tourism";
+	$dbname = "souvenir";
 	$conn = mysqli_connect($host, $user, $pass, $dbname) or die("Gagal");
 ?>
