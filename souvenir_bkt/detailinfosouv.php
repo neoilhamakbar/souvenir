38e74f6314597cc712870cd62df5f0576b4d101d
neoilhamakbar <50211523+neoilhamakbar@users.noreply.github.com>
2019-11-11 12:02:03 +0700

    Update detailinfosouv.php

--- souvenir_bkt/detailinfosouv.php
+++ souvenir_bkt/detailinfosouv.php
@@ -2,7 +2,7 @@
 require '../connect.php';
 $info = $_GET["info"];
 //$info = 'SO011';
-$querysearch ="select id, name, address,rating, owner,cp,ST_X(ST_Centroid(geom)) AS lng, ST_Y(ST_CENTROID(geom)) As lat from souvenir where id='$info'";	   
+$querysearch ="select id, name, address,rating,fasilitas_parkir,fasilitas_tempat_sholat, owner,cp,ST_X(ST_Centroid(geom)) AS lng, ST_Y(ST_CENTROID(geom)) As lat from souvenir where id='$info'";	   
 
 $hasil=mysqli_query($conn, $querysearch);
 while($row = mysqli_fetch_array($hasil))
@@ -11,11 +11,13 @@ while($row = mysqli_fetch_array($hasil))
 		  $name=$row['name'];
 		  $address=$row['address'];
 		  $rating=$row['rating'];
+		  $fasilitas_parkir=$row.['fasilitas_parkir'];
+		  $fasilitas_tempat_sholat=$row.['fasilitas_tempat_sholat'];
 		  $cp=$row['cp'];
 		  $owner=$row['owner'];
 		  $longitude=$row['lng'];
 		  $latitude=$row['lat'];
-		  $dataarray[]=array('id'=>$id,'name'=>$name,'address'=>$address,'rating'=>$rating,'owner'=>$owner,'cp'=>$cp,'longitude'=>$longitude,'latitude'=>$latitude);
+		  $dataarray[]=array('id'=>$id,'name'=>$name,'address'=>$address,'rating'=>$rating,'fasilitas_parkir'=>$fasilitas_parkir,'fasilitas_tempat_sholat'=>$fasilitas_tempat_sholat,'owner'=>$owner,'cp'=>$cp,'longitude'=>$longitude,'latitude'=>$latitude);
 	}
 echo json_encode ($dataarray);
 ?>
